437fdcac71306db4ebcc4529b5ec2f6595f3a91d
Torah <95339155+Termng@users.noreply.github.com>
2023-12-25 22:24:56 +0100

    base

--- website/auth.py
+++ website/auth.py
@@ -5,9 +5,9 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("about.html", text = "Hi Torah, this is a string expected to show like a react prop")
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -17,6 +17,6 @@ def logout():
     return "<p> Logout </p>"
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template("sign_up.html")
